93e569029887e4d5995acf21a437ea851bfedf67
Minouccc <343723899@qq.com>
2024-06-25 21:59:43 +0800

    feat(用户管理模块): 管理端用户列表页面

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Index } from "./container/Index";
+import { UserManage } from "./container/UserManage";
+import { ErrorPage } from "./container/ErrorPage";
+import { Login } from "./container/Login";
+import { Menu } from "./container/Menu";
+
+const routes = [
+  {
+    path: "/",
+    element: <Index></Index>,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Menu />,
+        children: [
+          {
+            path: "user_manage",
+            element: <UserManage />,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+];
+const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<RouterProvider router={router} />);
